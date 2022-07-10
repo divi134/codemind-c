675092eda649cd8dfd4bbae011d80dc68cc41db4
divi134 <96563541+divi134@users.noreply.github.com>
2022-07-10 14:25:35 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%c ",'A'-1+i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,20 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,arr[100],i,j;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=n;j++)
         {
-            if(j==1||j==i||i==n)
-            {
-                printf("*");
-            }
-            else
-            {
-                printf(" ");
-            }
+            printf("%c ",'A'-1+i);
         }
         printf("
 ");
